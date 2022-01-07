1bf142c94207bc3edd838f4d422da844bad2a964
JayGamao <jonreff.gamao@yahoo.com>
2022-01-07 11:52:17 +0800

    Fly

--- app/src/main/java/mcm/edu/ph/gamao_myfirstapp/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/gamao_myfirstapp/MainActivity.java
@@ -2,13 +2,20 @@ package mcm.edu.ph.gamao_myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
+
